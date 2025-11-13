8a38fe1e79c81ec66c90d41a0d7e47b14dd82025
Dave51C <dcoveney@verizon.net>
2025-11-13 11:58:49 -0500

    First attemp at adding module

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 """
 This is the main program. We'll import our other modules and run them here. 
 """
+import name
+Me = name.my_name()
+print ("My name is ",Me)
---
+++ name.py
@@ -0,0 +1,5 @@
+"""
+This module defines a function that returns "Dave"
+"""
+def my_name ():
+    return ('Dave')
